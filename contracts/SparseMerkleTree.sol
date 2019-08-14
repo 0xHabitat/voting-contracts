b21f33a4e11d5538c37147d7f1a036186a1864d6
Johann Barbie <johannbarbie@me.com>
2019-08-14 08:30:11 +0200

    added sparse merkle proofs

---
+++ contracts/SparseMerkleTree.sol
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2018-present, Leap DAO (leapdao.org)
+ *
+ * This source code is licensed under the Mozilla Public License, version 2,
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+pragma solidity >=0.4.21 <0.6.0;
+
+
+// Based on https://rinkeby.etherscan.io/address/0x881544e0b2e02a79ad10b01eca51660889d5452b#code
+contract SparseMerkleTree {
+
+  uint8 constant DEPTH = 9;
+  bytes32[DEPTH + 1] public defaultHashes;
+  bytes32 public root;
+
+  constructor() public {
+    // defaultHash[0] is being set to keccak256(uint256(0));
+    //defaultHashes[0] = 0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563;
+    for (uint8 i = 1; i <= DEPTH; i ++) {
+      defaultHashes[i] = keccak256(abi.encodePacked(defaultHashes[i-1], defaultHashes[i-1]));
+    }
+    root = defaultHashes[DEPTH];
+  }
+
+  function read(uint16 key, bytes32 leaf, bytes memory proof) public view returns (bool) {
+    bytes32 calculatedRoot = getRoot(leaf, key, proof);
+    return (calculatedRoot == root);
+  }
+
+  function write(uint16 key, bytes32 prevLeaf, bytes memory proof, bytes32 newLeaf) public {
+    bytes32 calculatedRoot = getRoot(prevLeaf, key, proof);
+    require(calculatedRoot == root, "update proof not valid");
+    root = getRoot(newLeaf, key, proof);
+  }
+
+  function del(uint16 key, bytes32 prevLeaf, bytes memory proof) public {
+    bytes32 calculatedRoot = getRoot(prevLeaf, key, proof);
+    require(calculatedRoot == root, "update proof not valid");
+    root = getRoot(defaultHashes[0], key, proof);
+  }
+
+  // first 160 bits of the proof are the 0/1 bits
+  function getRoot(bytes32 leaf, uint16 _index, bytes memory proof) public view returns (bytes32) {
+    require((proof.length - 2) % 32 == 0 && proof.length <= 290, "invalid proof format"); // 290 = 32 * 9 + 2
+    bytes32 proofElement;
+    bytes32 computedHash = leaf;
+    uint16 p = 2;  // length of trail
+    uint16 proofBits;
+    uint16 index = _index;
+    assembly {proofBits := div(mload(add(proof, 32)), exp(256, 30))} // 30 is number of bytes to shift 
+
+    for (uint d = 0; d < DEPTH; d++ ) {
+      if (proofBits % 2 == 0) { // check if last bit of proofBits is 0
+        proofElement = defaultHashes[d];
+      } else {
+        p += 32;
+        require(proof.length >= p, "proof not long enough");
+        assembly { proofElement := mload(add(proof, p)) }
+      }
+      if (index % 2 == 0) {
+        assembly {
+          mstore(0, computedHash)
+          mstore(0x20, proofElement)
+          computedHash := keccak256(0, 0x40)
+        }
+      } else {
+        assembly {
+          mstore(0, proofElement)
+          mstore(0x20, computedHash)
+          computedHash := keccak256(0, 0x40)
+        }
+      }
+      proofBits = proofBits / 2; // shift it right for next bit
+      index = index / 2;
+    }
+    return computedHash;
+  }
+}
---
+++ test/sparseMerkleTree.js
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2018-present, Leap DAO (leapdao.org)
+ *
+ * This source code is licensed under the Mozilla Public License, version 2,
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+const SparseMerkleTree = artifacts.require("SparseMerkleTree");
+const SmtLib = require('./helpers/SmtLib.js');
+
+contract("SparseMerkleTree", () => {
+
+  const leafOne = '0xa59a60d98b69a32028020fbf69c27dc2188b5756975e93b330a3f1513f383076';
+  const leafTwo = '0x95d22ccdd977e992e4a530ce4f1304e1a7a1840823ea1b4f7bf3841049d197e0';
+  const leafThree = '0x3d32085b3de13667b43fd7cecf200b347041918e259cbcc86796422a47fec794';
+  //const leafZero = '0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563';
+  const leafZero = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
+  it("should allow to verify proofs with single intersection", async() => {
+    const smt = await SparseMerkleTree.new();
+    const tree = new SmtLib(9, {
+        '353': leafOne,
+        '9': leafTwo,
+    });
+    let rsp = await smt.getRoot(leafOne, 353, tree.createMerkleProof(353));
+    assert.equal(rsp, tree.root);
+    rsp = await smt.getRoot(leafTwo, 9, tree.createMerkleProof(9));
+    assert.equal(rsp, tree.root);
+    rsp = await smt.getRoot(leafZero, 2, tree.createMerkleProof('2'));
+    assert.equal(rsp, tree.root);
+  });
+
+  it("should allow to update root - test1", async() => {
+    const smt = await SparseMerkleTree.new();
+
+    // write first leaf
+    let tree = new SmtLib(9);
+    await smt.write(10, leafZero, tree.createMerkleProof(10), leafOne);
+
+    // write second leaf
+    tree = new SmtLib(9, {10: leafOne});
+    await smt.write(9, leafZero, tree.createMerkleProof(9), leafTwo);
+
+    // read first leaf back
+    tree = new SmtLib(9, {
+      '10': leafOne,
+      '9': leafTwo,
+    });
+    let rsp = await smt.read(10, leafOne, tree.createMerkleProof(10));
+    assert(rsp);
+    // negative read test
+    rsp = await smt.read(10, leafTwo, tree.createMerkleProof(9));
+    assert(!rsp);
+    // read second leaf back
+    rsp = await smt.read(9, leafTwo, tree.createMerkleProof(9));
+    assert(rsp);
+
+    // delete test
+    await smt.del(10, leafOne, tree.createMerkleProof(10));
+
+    // try to read what is left
+    tree = new SmtLib(9, {
+      '9': leafTwo,
+    });
+    rsp = await smt.read(10, leafZero, tree.createMerkleProof(10));
+    assert(rsp, '10 not contained');
+    rsp = await smt.read(9, leafTwo, tree.createMerkleProof(9));
+    assert(rsp, '9 not contained');
+  });
+
+  it("should allow to update root - test2", async() => {
+    const smt = await SparseMerkleTree.new();
+
+    // write first leaf
+    let tree = new SmtLib(9);
+    await smt.write(308, leafZero, tree.createMerkleProof(308), leafOne);
+
+    // write second leaf
+    tree = new SmtLib(9, {308: leafOne});
+    await smt.write(9, leafZero, tree.createMerkleProof(9), leafTwo);
+
+    // read first leaf back
+    tree = new SmtLib(9, {
+      '308': leafOne,
+      '9': leafTwo,
+    });
+    let rsp = await smt.read(308, leafOne, tree.createMerkleProof(308));
+    assert(rsp);
+    // negative read test
+    rsp = await smt.read(308, leafTwo, tree.createMerkleProof(308));
+    assert(!rsp);
+
+    // delete test
+    await smt.del(308, leafOne, tree.createMerkleProof(308));
+    rsp = await smt.read(308, leafZero, tree.createMerkleProof(308));
+    assert(rsp);
+  });
+
+  it("The issue with the delete function", async() => {
+    const smt = await SparseMerkleTree.new();
+
+    // write first leaf
+    let tree = new SmtLib(9);
+    await smt.write(9, leafZero, tree.createMerkleProof(9), leafTwo);
+
+    const firstRoot = await smt.root();
+
+    tree = new SmtLib(9, {
+      '9': leafTwo,
+    });
+    // write second leaf
+    await smt.write(511, leafZero, tree.createMerkleProof(511), leafOne);
+
+    
+    tree = new SmtLib(9, {
+      '9': leafTwo,
+      '511': leafOne,
+    });
+    // read first leaf back
+    let rsp = await smt.read(9, leafTwo, tree.createMerkleProof(9));
+    assert(rsp);
+    // read second leaf
+    rsp = await smt.read(511, leafOne, tree.createMerkleProof(511));
+    assert(rsp);
+
+    // delete test
+    await smt.del(511, leafOne, tree.createMerkleProof(511));
+
+    tree = new SmtLib(9, {
+      '9': leafTwo,
+    });
+    const afterDeleteRoot = await smt.root();
+    assert.equal(firstRoot, afterDeleteRoot);
+    // After calling the delete function with one key, the root takes zerohashes value. So, others writes deletes too and you can't use read function.
+    rsp = await smt.read(9, leafTwo, tree.createMerkleProof(9));
+    assert(rsp, 'reading last value fails');
+  });
+
+});
